94e6fdf9e73a66df9f05c03b5e7032fb351bbad3
Kannan G <gkannan271120@gmail.com>
2023-03-15 11:02:57 +0530

    learning about jwt

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+app.use(express.json())
+
+const posts = [
+    {
+        username:'raja',
+        title:'post1'
+    },
+    {
+        username:'kanna',
+        title:'post2'
+    }
+]
+
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name))
+})
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if(token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user)=>{
+        if(err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
+
+app.listen(3000)
